0d0dbd181bf803fe234d1a588f9de533fa20c3be
jowallace1 <32683934+jowallace1@users.noreply.github.com>
2020-11-29 12:54:10 -0800

    Updating Files

--- Matlab & Simulink/ThrustCurve.m
+++ Matlab & Simulink/ThrustCurve.m
@@ -1,5 +1,4 @@
-fileName = "F15TC.xlsx";
-F15TC = zeros(27,2);
+fileName = "E12TC.csv";
 readMatrix = readmatrix(fileName);
-F15TC(2:27,1:2) = readMatrix(:,1:2);
-plot(F15TC(:,1),F15TC(:,2))
+TC = readMatrix(:,1:2);
+plot(TC(:,1),TC(:,2))
